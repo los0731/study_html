7238b2268496fcc3c165efefd54e182ff2aa8b43
Frank Park <los0731@gmail.com>
2018-10-21 10:45:24 +0900

    샘플 파서 코드 수정.

--- codevolve/HTML:CSS 2/parser_html/parser_html.py
+++ codevolve/HTML:CSS 2/parser_html/parser_html.py
@@ -2,24 +2,17 @@ from bs4 import BeautifulSoup
 
 with open('index.html', 'r') as file:
     soup = BeautifulSoup(file.read(), 'html.parser')
-    tag = soup.find('div', class_="nav-right").find_all('a')
+    tag = soup.find('div', class_="nav-left").find_all('a')
 
     assert(
         tag
-        and len(tag) == 4
-        and tag[0].text.strip() == 'Gmail'
-        and tag[1].text.strip() == 'Images'
-        and tag[2].find('i', class_="material-icons").text.strip() == 'apps'
-        and tag[2].find('i', class_="md-24").text.strip() == 'apps'
-        and tag[3].text.strip() == 'Sign in'
+        and len(tag) == 3
+        and tag[0].text.strip() == 'Privacy'
+        and tag[1].text.strip() == 'Terms'
+        and tag[2].text.strip() == 'Settings'
     )
 
     print("===========")
     print(tag)
-    print(len(tag))
-    print(tag[0].text.strip())
-    print(tag[1].text.strip())
-    print(tag[2].text.strip())
-    print(tag[3].text.strip())
     print("===========")
     
